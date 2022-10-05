854b24de6fc3d97b253007d3db32873bf8f86623
Lukas Fink <lukas.fink@troii.com>
2022-10-05 13:51:38 +0200

    s

---
+++ src/main/java/at/fhtw/slmmaintenancemonitor/SlmMaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmmaintenancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmMaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmMaintenanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/slmmaintenancemonitor/SlmMaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.slmmaintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmMaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
